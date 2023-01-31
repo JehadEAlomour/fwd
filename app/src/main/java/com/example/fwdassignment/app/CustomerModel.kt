457f2b6ac5cfe1cd67ef068c0b5122cb04b46e25
JehadEAlomour <1933345@std.hu.edu.jo>
2023-02-01 01:24:04 +0300

    fwd

---
+++ app/src/main/java/com/example/fwdassignment/app/CustomerModel.kt
@@ -0,0 +1,20 @@
+package com.example.fwdassignment.app
+
+data class CustomerModel
+    (
+    val Name: String,
+    val NumberPhone: String,
+    val TimeOrder: String,
+    val TimeAccept: String,
+    val Imag: String,
+    val CityName: String,
+    val StreetName: String,
+    val FloorNumber: Int,
+    val NumberBuilding: Int,
+    val DepartmentNumber: Int,
+    val NeighborhoodName: String,
+    val typeGas:String,
+    val Amount:Double,
+    val GasNumber:Int,
+    val paymentWay:Int
+    )
---
+++ app/src/main/java/com/example/fwdassignment/app/DriverModel.kt
@@ -0,0 +1,10 @@
+package com.example.fwdassignment.app
+
+data class DriverModel
+    (
+    val Image:String,
+    val Name:String,
+    val Address:String,
+    val NumberRequest:Int
+            )
+
